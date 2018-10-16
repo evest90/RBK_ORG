7002d6854966e322e3efbf6ba094d4657174727e
MohammadJozaa7 <m.jozaa@yahoo.com>
2018-10-16 10:52:07 +0300

    insert the data of graduates

--- info/graduates.js
+++ info/graduates.js
@@ -1,42 +1,67 @@
-var graduates =[
-    {
-      "name": "Joq Martin",
-      "occupation": "Senior Developer",
-      "company": "dasdas",
-      "image": "img/testimonial/client-1.jpg",
-      "qoute": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quas totam, delectus sit corporis quos error qui. Minus cupiditate explicabo minima ea neque perferendis corporis sint earum quas impedit",
-      "cohort_number": "cohort 4"
-    },
-    {
-      "name": "Joq Martin",
-      "occupation": "Senior Developer",
-      "company": "dasdas",
-      "image": "img/testimonial/client-2.jpg",
-      "qoute": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quas totam, delectus sit corporis quos error qui. Minus cupiditate explicabo minima ea neque perferendis corporis sint earum quas impedit",
-      "cohort_number": "cohort 4"
-    },
-    {
-      "name": "asd",
-      "occupation": "asd Developer",
-      "company": "dasdas",
-      "image": "img/testimonial/client-1.jpg",
-      "qoute": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quas totam, delectus sit corporis quos error qui. Minus cupiditate explicabo minima ea neque perferendis corporis sint earum quas impedit",
-      "cohort_number": "cohort 4"
-    },
-    {
-      "name": "3123123",
-      "occupation": "vd Developer",
-      "company": "dasdas",
-      "image": "img/testimonial/client-2.jpg",
-      "qoute": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quas totam, delectus sit corporis quos error qui. Minus cupiditate explicabo minima ea neque perferendis corporis sint earum quas impedit",
-      "cohort_number": "cohort 2"
-    },
-    {
-      "name": "sfdfsdf",
-      "occupation": "sad Developer",
-      "company": "dasdas",
-      "image": "img/testimonial/client-2.jpg",
-      "qoute": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quas totam, delectus sit corporis quos error qui. Minus cupiditate explicabo minima ea neque perferendis corporis sint earum quas impedit",
-      "cohort_number": "cohort 3"
-    }
-  ]
+var graduates = [
+  {
+    "name": "Rana Kilani",
+    "nationality": "Syrian",
+    "before": "Tenders manager in Hama, Syria",
+    "occupation": "Full Stack Developer",
+    "company": "Arabia Weather",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK is a life changing experience, it literally changed my life !",
+    "cohort_number": "Cohort 1"
+  },
+  {
+    "name": "Mohammad Albakri",
+    "nationality": "Jordanian",
+    "before": "Cashier",
+    "occupation": "Software Engineer",
+    "company": "BriteCore",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK definitely helped me to get my dream job !",
+    "cohort_number": "Cohort 1"
+  },
+  {
+    "name": "Rowaida Almehanni",
+    "nationality": "Yamani",
+    "before": "Unemployed",
+    "occupation": "Software Engineer",
+    "company": "Salalem",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK opened a new success way in my life !",
+    "cohort_number": "Cohort 2"
+  },
+  {
+    "name": "Ahmad Khasawneh",
+    "nationality": "Jordanian",
+    "before": "Unemployed",
+    "occupation": "Operating & Traffic Analyst",
+    "company": "Expedia",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK opens horizons that let you reach your imaginary dream job",
+    "cohort_number": "Cohort 2"
+  },
+  {
+    "name": "Atheer Mahmood",
+    "nationality": "Iraqi",
+    "before": "He was working as IT support in Iraq",
+    "occupation": "Full Stack Developer",
+    "company": "Codacity",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK is the best decision I’ve ever made in my life !",
+    "cohort_number": "Cohort 4"
+  },
+
+]
+/*
+sure you can add me if you want
+  {
+    "name": "Mohammad Jozaa",
+    "nationality": "Martian",
+    "before": "He was in madhouse",
+    "occupation": "Hacker in resident ()",
+    "company": "RBK",
+    "image": "img/testimonial/.jpg",
+    "quote": "RBK is the place that changed my entire life !",
+    "cohort_number": "Cohort 4"
+  },
+
+*/
