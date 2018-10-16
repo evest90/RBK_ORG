2574e97157840037eba34d84c501bbee5adecd85
Elyse Vest <evest90@gmail.com>
2018-10-16 14:18:53 +0300

    Attach images to urls for hiring partners except marhub

--- info/hiringPartners.js
+++ info/hiringPartners.js
@@ -4,84 +4,110 @@ var hiringPartners = [
     "url": "https://www.microsoft.com/en-jo"
   },
   {
-    "image": "img/client/mbc.png"
+    "image": "img/client/mbc.png",
+    "url": "http://www.mbc.net/en/corporate.html"
   },
   {
-    "image": "img/client/samsung.png"
+    "image": "img/client/samsung.png",
+    "url": "https://www.samsung.com/levant/"
   },
   {
-    "image": "img/client/zain.png"
+    "image": "img/client/zain.png",
+    "url": "https://www.zain.com/en/"
   },
   {
-    "image": "img/client/souq.png"
+    "image": "img/client/souq.png",
+    "url": "https://uae.souq.com/ae-en/"
   },
   {
-    "image": "img/client/arabiaWeather.png"
+    "image": "img/client/arabiaWeather.png",
+    "url": "http://en.arabiaweather.com/"
   },
   {
-    "image": "img/client/expedia.png"
+    "image": "img/client/expedia.png",
+    "url": "https://www.expedia.com/"
   },
   {
-    "image": "img/client/edraak.png"
+    "image": "img/client/edraak.png",
+    "url": "https://www.edraak.org/en/"
   },
   {
-    "image": "img/client/openSooq.png"
+    "image": "img/client/openSooq.png",
+    "url": "https://jo.opensooq.com/en"
   },
   {
-    "image": "img/client/britecore.png"
+    "image": "img/client/britecore.png",
+    "url": "https://www.britecore.com/"
   },
   {
-    "image": "img/client/friendture.png"
+    "image": "img/client/friendture.png",
+    "url": "https://www.friendture.com/"
   },
   {
-    "image": "img/client/codacity.png"
+    "image": "img/client/codacity.png",
+    "url": "https://www.codacity.com/"
   },
   {
-    "image": "img/client/akhtaboot.png"
+    "image": "img/client/akhtaboot.png",
+    "url": "https://www.akhtaboot.com/en/jordan"
   },
   {
-    "image": "img/client/jeeran.png"
+    "image": "img/client/jeeran.png",
+    "url": "http://jo.jeeran.com/amman/"
   },
   {
-    "image": "img/client/madfooatcom.png"
+    "image": "img/client/madfooatcom.png",
+    "url": "http://madfooat.com/"
   },
   {
-    "image": "img/client/sedco.png"
+    "image": "img/client/sedco.png",
+    "url": "https://www.sedco.co/"
   },
   {
-    "image": "img/client/shopGo.png"
+    "image": "img/client/shopGo.png",
+    "url": "http://www.shopgo.me/"
   },
   {
-    "image": "img/client/altibbi.png"
+    "image": "img/client/altibbi.png",
+    "url": "https://www.altibbi.com/"
   },
   {
-    "image": "img/client/dopravo.png"
+    "image": "img/client/dopravo.png",
+    "url": "https://www.dopravo.com/en/default"
   },
   {
-    "image": "img/client/dotMarket.png"
+    "image": "img/client/dotMarket.png",
+    "url": "https://www.dot.market/"
   },
   {
-    "image": "img/client/dreamTech.png"
+    "image": "img/client/dreamTech.png",
+    "url": "http://www.dreamtechs.co/"
   },
   {
-    "image": "img/client/etq.png"
+    "image": "img/client/etq.png",
+    "url": "https://www.etq.com/"
   },
   {
-    "image": "img/client/informaticaQatar.png"
+    "image": "img/client/informaticaQatar.png",
+    "url": "https://www.i-qatar.com/"
   },
   {
     "image": "img/client/marhub.png"
   },
   {
-    "image": "img/client/optimiza.png"
+    "image": "img/client/optimiza.png",
+    "url": "http://optimiza.me/"
   },
   {
-    "image": "img/client/reserveOut.png"
+    "image": "img/client/reserveOut.png",
+    "url": "https://www.reserveout.com/amman-en"
   },
   {
-    "image": "img/client/vardot.png"
+    "image": "img/client/vardot.png",
+    "url": "https://www.vardot.com/en-jo"
   },
   {
-    "image": "img/client/salalem.png"
+    "image": "img/client/salalem.png",
+    "url": "https://salalem.com/"
   },
 ]
